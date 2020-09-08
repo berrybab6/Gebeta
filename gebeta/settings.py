86b8bfceefca935cbd7878a0e1c7469dbd7d3613
berrybab6 <bedatuassefa@gmail.com>
2020-09-08 21:45:09 +0300

    add contact app to setting

--- gebeta/settings.py
+++ gebeta/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contact.apps.ContactConfig',
     'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
